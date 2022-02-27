a32f7452b9b6cc14ee35ecb25a398fb66419544b
Muhammad Ali <alinaeem229@gmail.com>
2022-02-26 20:21:14 -0500

    LED transitions

--- dino/dino.ino
+++ dino/dino.ino
@@ -1,39 +1,97 @@
 #include <FastLED.h>
-
-#define DATA_PIN 12
-#define NUM_LEDS 100
-CRGB leds[NUM_LEDS];
-
-
-
+#define LED_PIN_6 12
+#define LED_PIN_8 14
+#define LED_PIN_10 27
+#define NUM_LEDS_6 22
+#define NUM_LEDS_8 27
+#define NUM_LEDS_10 22
+#define BRIGHTNESS 5
+#define LED_TYPE WS2812
+#define COLOR_ORDER GRB
+CRGB leds_6[NUM_LEDS_6];
+CRGB leds_8[NUM_LEDS_8];
+CRGB leds_10[NUM_LEDS_10];
+#define UPDATES_PER_SECOND 2
+const int ldrPin = A0;
+CRGBPalette16 currentPalette;
+TBlendType currentBlending;
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
 void setup() {
-  Serial.begin(115200);
-  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  Serial.write("Lights turned on");
+delay( 3000 ); // power-up safety delay
+FastLED.addLeds<LED_TYPE, LED_PIN_6, COLOR_ORDER>(leds_6,
+NUM_LEDS_6).setCorrection( TypicalLEDStrip );
+FastLED.setBrightness( BRIGHTNESS );
+delay( 3000 ); // power-up safety delay
+FastLED.addLeds<LED_TYPE, LED_PIN_8, COLOR_ORDER>(leds_8,
+NUM_LEDS_8).setCorrection( TypicalLEDStrip );
+FastLED.setBrightness( BRIGHTNESS );
+delay( 3000 ); // power-up safety delay
+FastLED.addLeds<LED_TYPE, LED_PIN_10, COLOR_ORDER>(leds_10,
+NUM_LEDS_10).setCorrection( TypicalLEDStrip );
+FastLED.setBrightness( BRIGHTNESS );
+currentPalette = RainbowColors_p;
+currentBlending = LINEARBLEND;
+Serial.begin(115200);
+pinMode(ldrPin, INPUT);
 }
-
-void loop() {
-       // Move a single white led 
-   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed++) {
-      // Turn our current led on to white, then show the leds
-      leds[whiteLed] = CRGB::White;
-      leds[whiteLed+1] = CRGB::White;
-      leds[whiteLed+2] = CRGB::White;
-      leds[whiteLed+3] = CRGB::White;
-      leds[whiteLed+4] = CRGB::White;
-      leds[whiteLed+5] = CRGB::White;
-      // Show the leds (only one of which is set to white, from above)
-      FastLED.show();
-
-      // Wait a little bit
-      delay(100);
-
-      // Turn our current led back to black for the next loop around
-      leds[whiteLed] = CRGB::Black;
-      leds[whiteLed+1] = CRGB::Black;
-      leds[whiteLed+2] = CRGB::Black;
-      leds[whiteLed+3] = CRGB::Black;
-      leds[whiteLed+4] = CRGB::Black;
-      leds[whiteLed+5] = CRGB::Black;
-   }
+void loop()
+{
+int ldrStatus = analogRead(ldrPin);
+Serial.println(ldrStatus);
+delay(500);
+ChangePalettePeriodically();
+static uint8_t startIndex = 0;
+startIndex = startIndex + 1; /* motion speed */
+FillLEDsFromPaletteColors( startIndex);
+FastLED.show();
+FastLED.delay(1000 / UPDATES_PER_SECOND);
 }
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+uint8_t brightness = 255;
+for( int i = 0; i < NUM_LEDS_6; ++i) {
+leds_6[i] = ColorFromPalette( currentPalette, colorIndex, brightness,
+currentBlending);
+colorIndex += 3;
+}
+for( int i = 0; i < NUM_LEDS_8; ++i) {
+leds_8[i] = ColorFromPalette( currentPalette, colorIndex, brightness,
+currentBlending);
+colorIndex += 3;
+}
+for( int i = 0; i < NUM_LEDS_10; ++i) {
+leds_10[i] = ColorFromPalette( currentPalette, colorIndex, brightness,
+currentBlending);
+colorIndex += 3;
+}
+}
+void ChangePalettePeriodically()
+{
+uint8_t secondHand = (millis() / 1000) % 60;
+static uint8_t lastSecond = 99;
+if( lastSecond != secondHand) {
+lastSecond = secondHand;
+if( secondHand == 0) { currentPalette = RainbowColors_p;
+currentBlending = LINEARBLEND; }
+}
+}
+const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
+{
+CRGB::Red,
+CRGB::Gray, // 'white' is too bright compared to red and blue
+CRGB::Blue,
+CRGB::Black,
+CRGB::Red,
+CRGB::Gray,
+CRGB::Blue,
+CRGB::Black,
+CRGB::Red,
+CRGB::Red,
+CRGB::Gray,
+CRGB::Gray,
+CRGB::Blue,
+CRGB::Blue,
+CRGB::Black,
+CRGB::Black
+};
